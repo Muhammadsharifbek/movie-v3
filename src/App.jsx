47ba82cdc464aff245ec088c6354328e18fbde1d
Sharif <sharifahmadov690@gmail.com>
2024-05-22 23:21:52 +0500

    add loader

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
   ]);
   const [term, setTerm] = useState("");
   const [filter, setFilter] = useState("all");
+  const [isLoading, setIsLoading] = useState(false);
 
   const addMovie = (movie) => {
     const newItem = { title: movie.title, viewers: movie.viewers, id: uuidv4() };
@@ -27,20 +28,6 @@ const App = () => {
     const newArr = data.filter((movie) => movie.id !== id);
     setData(newArr);
   };
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
-      .then((responce) => responce.json())
-      .then((json) => {
-        const newArr = json.map((movie) => ({
-          title: movie.title,
-          id: uuidv4(),
-          viewers: movie.id * 10,
-        }));
-        setData(newArr);
-      })
-      .catch((err) => console.log(err));
-  }, []);
 
   const handleSearch = (arr, term) => {
     if (term.length === 0) {
@@ -62,6 +49,22 @@ const App = () => {
   const updateTermInput = (term) => setTerm(term);
   const updateFilterInput = (filter) => setFilter(filter);
 
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+      .then((responce) => responce.json())
+      .then((json) => {
+        const newArr = json.map((movie) => ({
+          title: movie.title,
+          id: uuidv4(),
+          viewers: movie.id * 10,
+        }));
+        setData(newArr);
+      })
+      .catch((err) => console.log(err))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div>
       <h2>
@@ -70,7 +73,7 @@ const App = () => {
 
         <Search updateTermInput={updateTermInput} />
         <FilterMovie visibleCount={data.length} updateFilterInput={updateFilterInput} filter={filter} />
-
+        {isLoading && <h2>...loading</h2>}
         <Movie deleteButton={deleteButton} data={updateFilter(handleSearch(data, term), filter)} />
         <NewMovie addMovie={addMovie} />
       </h2>
